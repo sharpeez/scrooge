aaddb69494c05da53d809afe531093fdb906f398
Adon Metcalfe <adon.metcalfe@gmail.com>
2017-09-14 00:01:35 +0800

    add reversion, ignore static

--- scrooge/admin.py
+++ scrooge/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
 from scrooge.models import ContractReference, Cost
 
-admin.site.register([ContractReference, Cost])
+@admin.register(ContractReference)
+class ContractReferenceAdmin(VersionAdmin):
+    pass
+
+@admin.register(Cost)
+class CostAdmin(VersionAdmin):
+    pass
