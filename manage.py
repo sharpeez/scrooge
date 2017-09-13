8d6b2fce0e20a089f8a751863dc0b01855132868
dpawasi <asi@dpaw.wa.gov.au>
2017-09-13 15:20:04 +0800

    butchering

--- manage.py
+++ manage.py
@@ -5,9 +5,8 @@ import sys
 
 confy.read_environment_file()
 
-
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authome.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scrooge.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
