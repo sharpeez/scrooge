ddfff904382d65231b356a64a1edac83f39cb7ab
Adon Metcalfe <adonm@fastmail.fm>
2017-09-17 22:41:35 +0800

    import export

--- scrooge/admin.py
+++ scrooge/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from reversion.admin import VersionAdmin
-from scrooge.models import ContractReference, Cost, CostBreakdown, ITSystem, UserGroup
+from import_export import resources
+from import_export.admin import ImportExportMixin
+from scrooge.models import Contract, Cost, CostBreakdown, ITSystem, UserGroup
 
-@admin.register(ContractReference)
-class ContractReferenceAdmin(VersionAdmin):
+class ScroogeAdmin(ImportExportMixin, VersionAdmin):
+    pass
+
+@admin.register(Contract)
+class ContractAdmin(ScroogeAdmin):
     list_editable = ['invoice_period', 'start', 'end']
     list_display = ['__str__', 'brand'] + list_editable
     search_fields = ['vendor', 'contract', 'brand']
@@ -11,18 +16,21 @@ class ContractReferenceAdmin(VersionAdmin):
     list_filter = ["vendor", "invoice_period"]
 
 @admin.register(Cost)
-class CostAdmin(VersionAdmin):
+class CostAdmin(ScroogeAdmin):
+    list_editable = ["actual_cost", "predicted_cost"]
+    list_display = ["__str__", "contract", "quantity"] + list_editable + ["description", "comment"]
     list_filter = ["finyear", "allocated_percentage"]
+    search_fields = ["name", "description", "comment", "contract__vendor", "contract__contract", "contract__brand"]
 
 @admin.register(ITSystem)
-class ITSystemAdmin(VersionAdmin):
+class ITSystemAdmin(ScroogeAdmin):
     pass
 
 @admin.register(UserGroup)
-class UserGroupAdmin(VersionAdmin):
+class UserGroupAdmin(ScroogeAdmin):
     pass
 
 @admin.register(CostBreakdown)
-class CostBreakdownAdmin(VersionAdmin):
+class CostBreakdownAdmin(ScroogeAdmin):
     list_filter = ["service_pool"]
     filter_horizontal = ["it_systems", "user_groups"]
--- scrooge/models.py
+++ scrooge/models.py
@@ -2,6 +2,7 @@ from datetime import date
 from django.db import models
 from django.core.validators import MaxValueValidator
 from django.contrib.postgres.fields import JSONField
+from django.core.exceptions import ValidationError
 
 FINYEAR_CHOICES = (
     (2015, '2015/16'),
@@ -11,29 +12,13 @@ FINYEAR_CHOICES = (
     (2019, '2019/20')
 )
 
-class ContractReference(models.Model):
+class Contract(models.Model):
     vendor = models.CharField(max_length=320)
-    brand = models.CharField(max_length=320, blank=True)
-    contract = models.CharField(max_length=320)
+    brand = models.CharField(max_length=320, default="N/A")
+    contract = models.CharField(max_length=320, default="N/A")
     invoice_period = models.CharField(max_length=320, default="Annual")
     start = models.DateField(default=date.today)
     end = models.DateField(null=True, blank=True)
-
-    @classmethod
-    def grab(cls, costdict):
-        from datetime import datetime
-        def parsedate(text):
-            return datetime.strptime(text, "%d-%b-%y").date()
-        contractvalues = {"vendor": costdict["Vendor"], "contract": costdict["Contract Reference"]}
-        contract, created = cls.objects.get_or_create(**contractvalues)
-        try:
-            contract.start = parsedate(costdict["Start"])
-            contract.end = parsedate(costdict["End"])
-        except:
-            contract.invoice_period = costdict["Start"]
-        contract.save()
-        return contract
-
 
     def __str__(self):
         return "{} ({})".format(self.vendor, self.contract)
@@ -42,40 +27,22 @@ class ContractReference(models.Model):
         unique_together = ("vendor", "contract")
 
 class Cost(models.Model):
-    contract = models.ForeignKey(ContractReference)
+    contract = models.ForeignKey(Contract)
     name = models.CharField(max_length=320, help_text="Product or Service")
     description = models.TextField(blank=True)
-    comment = models.TextField(blank=True)
+    comment = models.TextField(blank=True, default="")
     quantity = models.CharField(max_length=320, default="1")
     finyear = models.IntegerField(choices=FINYEAR_CHOICES)
     actual_cost = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
     predicted_cost = models.DecimalField(max_digits=12, decimal_places=2, null=True)
-    allocated_percentage = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
+    allocated_percentage = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)], editable=False)
 
-    @classmethod
-    def import_csv(cls, filename):
-        import csv
-        from decimal import Decimal
-        costs = csv.DictReader(open(filename))
-        for costdict in costs:
-            contract = ContractReference.grab(costdict)
-            try:
-                proj = Decimal(costdict["Projected Cost FY 17/18"].replace(",", ""))
-            except:
-                proj = Decimal(0)
-            contractvalues = {
-                "name": costdict["Product/Service"],
-                "finyear": 2017,
-                "contract": contract
-            }
-            cost, created = Cost.objects.get_or_create(**contractvalues)
-            cost.description = costdict["Description"]
-            cost.quantity = costdict["Quantity"]
-            cost.predicted_cost = proj
-            cost.comment = costdict["Comment"]
-            cost.save()
-            print("{}: {}".format(contract, cost))
-    
+    def calc_allocated_percentage(self):
+        total = 0
+        for pct in self.cost_breakdown_set.values("percentage"):
+            total += pct[0]
+        return total
+
     class Meta:
         unique_together = ("name", "finyear", "contract")
 
@@ -115,8 +82,12 @@ class CostBreakdown(models.Model):
     description = models.TextField(blank=True)
     service_pool = models.CharField(max_length=64, choices=SERVICE_POOL_CHOICES)
     percentage = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
-    it_systems = models.ManyToManyField(ITSystem)
-    user_groups = models.ManyToManyField(UserGroup)
+    it_systems = models.ManyToManyField(ITSystem, blank=True, editable=False)
+    user_groups = models.ManyToManyField(UserGroup, blank=True, editable=False)
+
+    def clean(self):
+        if self.percentage + self.cost.allocated_percentage > 100:
+            raise ValidationError("Total allocated percentage can't exceed 100")
 
     def __str__(self):
         return self.name
